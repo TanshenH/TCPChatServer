a1a0aebd372e17dc545cbfc415be54cf58b32b61
0xT.H <172159874+TanshenH@users.noreply.github.com>
2025-08-01 15:42:11 +0530

    Add files via upload

---
+++ client.c
@@ -0,0 +1,47 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define PORT 1337
+#define IP_ADDR "127.0.0.1" // localhost
+
+int main() {
+
+  int clientFd;
+  char recvBuffer[256]; // receive server message
+  char sendBuffer[256]; // send server message
+  struct sockaddr_in serverAddr;
+
+  memset(recvBuffer, 0, 256); // set all values to 0
+  memset(sendBuffer, 0, 256); // this too
+
+  // initialize the socket
+  clientFd = socket(AF_INET, SOCK_STREAM, 0);
+
+  // declare the server information
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(PORT);
+  serverAddr.sin_addr.s_addr = inet_addr(IP_ADDR);
+
+  // connect the socket with the server
+  if (connect(clientFd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) <
+      0) {
+    perror("Connect failed");
+    return EXIT_FAILURE;
+  }
+  while (1) {
+    printf("Message : ");
+    fgets(sendBuffer, 256, stdin);
+    send(clientFd, sendBuffer, strlen(sendBuffer), 0); // sends to server
+    recv(clientFd, recvBuffer, sizeof(recvBuffer), 0); // receive from server
+    printf("Server : %s", recvBuffer);
+  }
+
+  close(clientFd);
+  return EXIT_SUCCESS;
+}
---
+++ server.c
@@ -0,0 +1,78 @@
+#include <arpa/inet.h>  // inet functions
+#include <netinet/in.h> // struct sockaddr_in struture
+#include <stdio.h>      // logging
+#include <stdlib.h>     // system related func
+#include <string.h>     // mem related funcs
+#include <sys/socket.h> // socket
+#include <sys/types.h>  // for more datatypes
+#include <unistd.h>     // close()
+
+#define PORT 1337 // port 1337 , hehehe elite coder !
+#define MAX_CLIENT 10
+
+int main() {
+
+  char recvBuffer[256]; // receive from clients
+  char sendBuffer[256]; // send to clients
+  int serverFd;
+  int clientFd;
+  int clientNum = 0;
+  int checkRecv;
+  struct sockaddr_in serverAddr;
+  struct sockaddr clientAddr;
+  socklen_t clientAddr_len = sizeof(clientAddr);
+
+  memset(&recvBuffer, 0, 256); // set all values to 0
+  memset(&sendBuffer, 0, 256); // this too
+
+  // initialize the server socket : like make it socket
+  serverFd = socket(AF_INET, SOCK_STREAM, 0);
+
+  // declare the server socket information
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(PORT);
+  serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+  if (bind(serverFd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+    perror("Bind failed");
+    return EXIT_FAILURE;
+  }
+
+  printf("Number of client connected : %d\n", clientNum);
+  while (1) {
+    // wait for clients
+    listen(serverFd, MAX_CLIENT);
+    clientFd = accept(serverFd, &clientAddr,
+                      &clientAddr_len); // accept the incoming connection
+    if (clientFd < 0) {
+      perror("accept failed");
+      continue;
+    }
+    clientNum++;
+    printf("Number of client connected : %d\n", clientNum);
+
+    while (1) {
+      memset(recvBuffer, 0, 256);
+      // receive message from client
+      checkRecv = recv(clientFd, recvBuffer, sizeof(recvBuffer), 0);
+      if (checkRecv == 0) {
+        printf("Client Disconnected\n");
+        break;
+      } else if (checkRecv < 0) {
+        perror("Recv failed");
+        break;
+      }
+
+      printf("Client%d : %s", clientNum, recvBuffer);
+      printf("Message : ");
+      fgets(sendBuffer, 256, stdin);
+      send(clientFd, sendBuffer, strlen(sendBuffer),
+           0); // send message to client
+    }
+    clientNum--;
+    close(clientFd);
+    printf("\nNumber of client connected : %d\n", clientNum);
+  }
+  close(serverFd);
+  return EXIT_SUCCESS;
+}
